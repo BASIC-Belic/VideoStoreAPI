8ecb6f08ffb6b490e5d13e6eea26aaadd76c4c8a
Kat Perreira <kat.perreira@gmail.com>
2018-11-05 14:59:17 -0800

    set up controllers for movies and customers

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -20,14 +20,13 @@ describe Movie do
       @customer = Customer.find_by(id: customer_id)
     end
 
-    it 'each movie can have 1 customer'do
-
+    it 'each movie can have a customer'do
     expect(@movie2.customer_ids.include? @customer.id).must_equal true
   end
 
 
   it 'each movie can have 0 customers' do
-    
+
     expect(@movie).must_respond_to  :customers
     expect(@movie.customers.length).must_equal 0
 
