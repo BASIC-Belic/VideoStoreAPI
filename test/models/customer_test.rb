7fcabce3503610109b89496c671b7aed7000a9c3
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-05 12:22:29 -0800

    relations tests for movies

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -10,9 +10,23 @@ describe Customer do
     describe 'relations' do
       it "has 0 or 1 movies" do
         expect(@user).must_respond_to :movies
-        # @user.items.each do |item|
-        #   expect(item).must_be_kind_of Item
-        # end
+        @user.movies.each do |movie|
+          expect(movie).must_be_kind_of Movie
+        end
+      end
+
+      it 'belongs to 0 or 1 movies' do
+        movie_ids = Customer.first.movie_ids
+        movie_ids.each do |id|
+          movie = Movie.find_by(id: id)
+          expect(movie).must_be_kind_of Movie
+        end
+
+        customer_ids = Movie.first.customer_ids
+        customer_ids.each do |id|
+          customer = Customer.find_by(id: customer_id)
+          expect(customer).must_be_kind_of Customer
+        end
       end
     end
   end
