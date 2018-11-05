1ff3f0200ec118e90a94887e2afafb87c3988f92
Kat Perreira <kat.perreira@gmail.com>
2018-11-05 14:31:24 -0800

    movie relation tests pass

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,38 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  describe 'relations' do
+
+    before do
+      # set up movie variables
+      @movie = Movie.first
+      @movie2 = Movie.last
+      @movie.release_date = Date.today
+      @movie.save
+      @movie2.release_date = Date.today
+      @movie2.save
+
+      @customer = Customer.first
+      @movie2.customer_ids << @customer.id
+      # customer variable
+      customer_id = @movie2.customer_ids[0]
+      @customer = Customer.find_by(id: customer_id)
+
+
+      expect(@movie2.customer_ids.include? @customer.id).must_equal true
+    end
+
+    it 'each movie can have 0 customers' do
+
+      expect(@movie).must_respond_to  :customers
+      expect(@movie.customers.length).must_equal 0
+
+    end
+
+
+
+
   end
+
 end
