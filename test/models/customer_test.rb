a2e1745d6f3be828d12db9eb9d7cede8cac02d59
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-05 14:23:00 -0800

    relations test

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -5,19 +5,31 @@ describe Customer do
 
     before do
       @user = Customer.first
-      @user.registered_at = DateTime.today
+      @user.registered_at = DateTime.now
       @user.save
+
+      @no_movies_user = Customer.find_by(movies.length == 0)
+      @no_movies_user.registered_at = DateTime.now
+      @no_movies_user.save
     end
 
     describe 'relations' do
-      it "has 0 or 1 movies" do
+
+      it "can have 0 movies" do
+        expect(@no_movies_user.valid?).must_equal true
+        expect(@no_movies_user).must_respond_to :movies
+        expect(@no_movies_user.movies.empty?).must_equal true
+      end
+
+      it "can have 1 or more movies" do
+        expect(@user.valid?).must_equal true
         expect(@user).must_respond_to :movies
         @user.movies.each do |movie|
           expect(movie).must_be_kind_of Movie
         end
       end
 
-      it 'belongs to 0 or 1 movies' do
+      it 'belongs to 0 or many movies' do
         movie_ids = Customer.first.movie_ids
         movie_ids.each do |id|
           movie = Movie.find_by(id: id)
