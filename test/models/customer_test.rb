ee2baa54d0c4106102d6762184c3d5c2c7bba6f2
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-05 14:51:47 -0800

    tests are passing and testing the correct thing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -4,11 +4,16 @@ describe Customer do
   describe Customer do
 
     before do
-      # @user = Customer.first
-      # @user.registered_at = DateTime.now
-      # @user.save
+      @user = Customer.first
 
-      @no_movies_user = Customer.first
+      movie_id = Movie.first.id
+      @movie = Movie.first
+      @user.movies << @movie
+
+      @user.registered_at = DateTime.now
+      @user.save
+
+      @no_movies_user = Customer.last
       @no_movies_user.registered_at = DateTime.now
       @no_movies_user.save
     end
@@ -22,33 +27,30 @@ describe Customer do
       end
 
       it "can have 1 or more movies" do
-        # movie_id = Movie.first.id
-        # movie = Movie.find_by(id: movie_id)
-        # @user.movie_ids << movie_id
-        #
-        # expect(@user.valid?).must_equal true
-        # expect(@user).must_respond_to :movies
-        # expect(@user.movies.length).must_equal 1
-        #
-        # @user.movies.each do |movie|
-        #   expect(movie).must_be_kind_of Movie
-        #   expect(movies.include?(movie))
-        # end
+
+        expect(@user.valid?).must_equal true
+        expect(@user).must_respond_to :movies
+        expect(@user.movies.length).must_equal 1
+
+        @user.movies.each do |movie|
+          expect(movie).must_be_kind_of Movie
+          expect(movies.include?(@movie))
+        end
       end
 
-      # it 'belongs to 0 or many movies' do
-      #   movie_ids = Customer.first.movie_ids
-      #   movie_ids.each do |id|
-      #     movie = Movie.find_by(id: id)
-      #     expect(movie).must_be_kind_of Movie
-      #   end
-      #
-      #   customer_ids = Movie.first.customer_ids
-      #   customer_ids.each do |id|
-      #     customer = Customer.find_by(id: customer_id)
-      #     expect(customer).must_be_kind_of Customer
-      #   end
-      # end
+      it 'belongs to 0 or many movies' do
+        movie_ids = @user.movie_ids
+        movie_ids.each do |id|
+          movie = Movie.find_by(id: id)
+          expect(movie).must_be_kind_of Movie
+        end
+
+        customer_ids = @movie.customer_ids
+        customer_ids.each do |id|
+          customer = Customer.find_by(id: id)
+          expect(customer).must_be_kind_of Customer
+        end
+      end
     end
 
     describe 'validations' do
