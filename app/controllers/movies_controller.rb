e8750e97bd0f61eb521ac5a624dddf18a3222314
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-05 16:56:00 -0800

    create in movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,26 +3,34 @@ class MoviesController < ApplicationController
   def index
     movies = Movie.all
 
-        if movies
-          render json: jsonify(movies), status: :ok
-        else
-          render_errors(:not_found, { movies: ["No movies found."]})
-        end
+    if movies
+      render json: jsonify(movies), status: :ok
+    else
+      render_errors(:not_found, { movies: ["No movies found."]})
+    end
   end
 
   def show
   end
 
   def create
+    movie = Movie.new(movie_params)
+    
+    if movie.save
+      render json: { id: movie.id }
+    else
+      render_errors(:bad_request, movie.errors.messages)
+    end
   end
 
   private
 
-    def jsonify(movie_data)
-      return movie_data.as_json(only: [:title, :overview,  :release_date, :inventory, ])
-    end
+  def jsonify(movie_data)
+    return movie_data.as_json(only: [:title, :overview, :release_date, :inventory])
+  end
+
   def movie_params
-    params.require(:movie).permit(:title)
+    params.require(:movie).permit(:title, :release_date, :overview, :inventory)
   end
 
 end
