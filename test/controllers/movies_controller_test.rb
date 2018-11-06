f24a618d6d4e4183a8ae6392ff1c165f749362e0
Kat Perreira <kat.perreira@gmail.com>
2018-11-06 12:03:01 -0800

    this will be lost in the merge

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -4,4 +4,14 @@ describe MoviesController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+
+
+
+
+
+  it "returns an empty array when there are no movies" do
+
+    Pets.all = Pets.destroy
+
 end
