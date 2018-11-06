d668538af2cf44e3734086b5eb9d10c9bf129f2c
Kat Perreira <kat.perreira@gmail.com>
2018-11-05 16:04:43 -0800

    merge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,4 @@
 class Movie < ApplicationRecord
-  has_many :customers, :through => :rentals
-<<<<<<< HEAD
-
   has_many :rentals
-
-=======
   has_many :rentals
->>>>>>> 330f92fd289d4c776758cd86d760b00e27ed86d8
 end
