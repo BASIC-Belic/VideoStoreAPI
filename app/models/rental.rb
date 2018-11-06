69bbfd4b9032c6c83b0defa22e762b3dff6dd440
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-06 14:48:17 -0800

    method for creating due date

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,18 @@
 class Rental < ApplicationRecord
   belongs_to :movie
   belongs_to :customer
+  after_initialize :create_due_date
+  after_initialize :create_checkout_date
+  #/rentals/check-out
+
+
+  private
+
+  def create_checkout_date
+    self.checkout_date = Date.today
+  end
+
+  def create_due_date
+    self.due_date = Date.today + 7
+  end
 end
