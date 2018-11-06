2af46c6380223f26ba8880fdf00b4ad81d8fa356
Kat Perreira <kat.perreira@gmail.com>
2018-11-06 12:30:25 -0800

    customer test index all tests passing

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
-
+# :id, :name, :address, :city, :state, :postal_code, :phone, :registered_at])
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  CUSTOMER_FIELDS = %w(id name address city state postal_code phone registered_at).sort!
+
+  # helper method to refactor the json
+
+  def check_response(expected_type, expected_status: :success)
+    must_respond_with :success
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+
+    return body
+
+  end
+
+  describe "index" do
+
+    it "is a real working route" do
+      get customers_path
+
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Array
+      expect(body.length).must_equal Customer.count
+
+      body.each do |customer|
+        expect(customer.keys.sort).must_equal CUSTOMER_FIELDS
+      end
+
+
+
+    end
+
+
+  end
+
 end
