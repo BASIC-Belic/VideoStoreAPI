91bb474e775c372213d5a325912f9a892ed87565
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-07 14:47:33 -0800

    commented out tests to pull

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -2,17 +2,24 @@ require "test_helper"
 
 describe Movie do
 
+  before do
+    @movie = Movie.first
+    @movie2 = Movie.last
+    @customer = Customer.first
+    @rental = Rental.create(movie: @movie, customer: @customer)
+
+    #
+    # @empty_movie = Movie.find{ |r| r.rentals.length == 0 }
+    # @full_movie = Movie.find{ |r| r.rentals.length == r.inventory }
+    # @semi_full_movie = Movie.find{ |r|
+    #    (r.rentals.length != r.inventory) && (r.rentals.length >= 1) }
+
+  end
+
+  describe 'validations' do
+  end
+
   describe 'relations' do
-
-    before do
-
-      @movie = Movie.first
-      @movie2 = Movie.last
-      @customer = Customer.first
-      @rental_empty = Rental.create
-      @rental = Rental.create(movie: @movie, customer: @customer)
-
-    end
     #
     # it 'each movie can have 0 rentals'
     # expect(@movie2).must_respond_to :rentals
@@ -22,12 +29,25 @@ describe Movie do
 
       expect(@movie).must_respond_to  :rentals
       expect(@movie.rentals.length).must_equal 1
-
     end
-
-
-
-
   end
 
+  describe 'available_inventory' do
+
+    # it 'will return 0 if movie has no available inventory' do
+    #   expect(@empty_movie.available_inventory).must_equal 0
+    # end
+    #
+    # it 'will return correct available inventory if some rentals on movie' do
+    #   expect(@semi_full_movie.available_inventory).wont_equal inventory
+    #   expect(@semi_full_movie.available_inventory).must_be :>=, @semi_full_movie.inventory
+    # end
+    #
+    # it 'will return value inventory length if all movies have current rentals' do
+    #   expect(@full_movie.available_inventory).must_equal @full_movie.inventory
+    # end
+    #
+    # it 'will return [something] for negative???' do
+    # end
+  end
 end
