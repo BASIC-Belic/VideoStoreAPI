eee0b35a8d70f9defb988c45ccea9941e135876a
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-07 14:07:07 -0800

    rentals available_inventory updated

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,11 @@
 class Movie < ApplicationRecord
   has_many :rentals, dependent: :nullify
-
   validates :title, presence: true
 
   def available_inventory
-    return self.inventory - self.rentals_count
+     num_curr_rentals = self.rentals.select {
+       |rental| rental.checked_out == true
+     }
+     return self.inventory - num_curr_rentals
   end
 end
