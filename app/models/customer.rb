ca8bd4537ad774221bb34de92763b6dfa0af4cc2
Kat Perreira <kat.perreira@gmail.com>
2018-11-09 14:52:06 -0800

    devin magic, tests work, i'm done with tests, what the actual helllllllll

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,15 +3,17 @@ class Customer < ApplicationRecord
   has_many :rentals, dependent: :nullify
   validates :name, presence: true
 
+  # def movies_checked_out_count
+  #   find_checked_out_count
+  # end
+  #
+  # private
+
   def movies_checked_out_count
-    find_checked_out_count
-  end
-
-  private
-
-  def find_checked_out_count
     return self.rentals.select {
       |rental| rental.checked_out == true
     }.length
   end
+
+  private
 end
